389849ea6af2b0a74621573b8599aa5057aafb17
jayadamsmorgan <applejay1802@gmail.com>
2017-08-16 22:48:42 -0400

    minor changes

--- app/src/main/java/com/berdnikovllc/unitedsocialnetwork/united/MainActivity.java
+++ app/src/main/java/com/berdnikovllc/unitedsocialnetwork/united/MainActivity.java
@@ -1,9 +1,9 @@
 package com.berdnikovllc.unitedsocialnetwork.united;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,25 +17,37 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    FloatingActionButton fabNewPost, fabNewMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("Feed");
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fabNewPost = findViewById(R.id.fab_new_post);
+        fabNewPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NewPostActivity.class);
+                startActivity(intent);
+            }
+        });
+        fabNewMessage = findViewById(R.id.fab_new_message);
+        fabNewMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        SelectReceiversActivity.class);
+                startActivity(intent);
             }
         });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -78,21 +90,31 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Handling navigation view item clicks.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_feed) {               // FEED
+            fabNewMessage.hide();
+            fabNewPost.show();
+            setTitle(R.string.feed);
+        } else if (id == R.id.nav_messages) {    // MESSAGES
+            fabNewPost.hide();
+            fabNewMessage.show();
+            setTitle(R.string.messages);
+        } else if (id == R.id.nav_photos) {      // PHOTOS
+            fabNewMessage.hide();
+            fabNewPost.hide();
+            setTitle(R.string.photos);
+        } else if (id == R.id.nav_settings) {    // SETTINGS
+            fabNewMessage.hide();
+            fabNewPost.hide();
+            setTitle(R.string.settings);
+        } else if (id == R.id.nav_send_bug) {    // BUGREPORT
+            fabNewPost.hide();
+            fabNewMessage.hide();
+            Intent intent = new Intent(MainActivity.this,
+                    SendBugReportActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/berdnikovllc/unitedsocialnetwork/united/SelectReceiversActivity.java
@@ -0,0 +1,13 @@
+package com.berdnikovllc.unitedsocialnetwork.united;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectReceiversActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_receivers);
+    }
+}
